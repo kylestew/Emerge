44009b5a696c5626c49bade550f71bf7357f899e
Kyle Stewart <kylestew@gmail.com>
2016-01-02 14:25:57 -0800

    ...

---
+++ Emerge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emerge
+//
+//  Created by Kyle Stewart on 1/2/16.
+//  Copyright © 2016 Kyle Stewart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
