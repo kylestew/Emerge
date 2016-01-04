5dfde84cf48cce2b9fc80d1b03862631fe0fdfc5
Kyle Stewart <kylestew@gmail.com>
2016-01-03 16:50:42 -0800

    Including custom textview class for code editing

---
+++ Emerge/CodeEditorViewController.swift
@@ -0,0 +1,32 @@
+import UIKit
+
+class CodeEditorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+//        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+//        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+//        
+//        QEDTextView *textView = [[QEDTextView alloc] initWithFrame:self.view.bounds];
+//        textView.autoresizingMask = UIViewAutoresizingFlexibleWidth|UIViewAutoresizingFlexibleHeight;
+//        textView.delegate = self;
+//        
+//        self.textView = textView;
+//        
+//        [self.view addSubview:textView];
+//        
+//        self.textView.text = @"// Test comment\n\n"\
+//        @"// Let's solve our first equation\n"\
+//        @"x = 1 / (1 + x) // equation to solve for x\n"\
+//        @"x > 0 // restrict to positive root\n\n"\
+//        @"// Let's create a user function\n"\
+//        @"// The standard function random returns a random value between 0 an 1\n"\
+//        @"g(x) := 0.005 * (x + 1) * (x - 1) + 0.1 * (random - 0.5)\n\n"\
+//        @"// Now let's plot the two functions together on one chart\n"\
+//        @"plot f(x), g(x)";
+        
+    }
+
+}
